bbb51b5e80f580d7ec7b1008b085c26a604d0704
Nawin Sundaraketu <nawin.sun@no-tus.com>
2024-09-03 01:59:57 +0700

    change images

--- src/app/data/home/skillGroup.js
+++ src/app/data/home/skillGroup.js
@@ -2,25 +2,23 @@ import Image from "next/image";
 
 import jsLogo from "../../../../public/skill/programming-language/js-logo.png";
 import pythonLogo from "../../../../public/skill/programming-language/python-logo.png";
-import phpLogo from "../../../../public/skill/programming-language/php-logo.png";
-import javaLogo from "../../../../public/skill/programming-language/java-logo.png";
-import tsLogo from "../../../../public/skill/programming-language/typescript-logo.png";
+import javaLogo from "../../../../public/skill/programming-language/java-logo.jpg";
+import tsLogo from "../../../../public/skill/programming-language/ts-logo.png";
 
-import htmlLogo from "../../../../public/skill/front-end/html-logo.jpg";
+import htmlLogo from "../../../../public/skill/front-end/html-logo.png";
 import cssLogo from "../../../../public/skill/front-end/css-logo.png";
 import reactLogo from "../../../../public/skill/front-end/react-logo.png";
-import nextLogo from "../../../../public/skill/front-end/next-logo.webp";
+import nextLogo from "../../../../public/skill/front-end/nextjs-logo.png";
 
 import nestLogo from "../../../../public/skill/back-end/nest-logo.svg";
-import expressLogo from "../../../../public/skill/back-end/express-logo.jpg";
+import expressLogo from "../../../../public/skill/back-end/express-logo.png";
 import nodeLogo from "../../../../public/skill/back-end/nodejs-logo.png";
 
 import postgressLogo from "../../../../public/skill/database/postgress-logo.png";
-import mongodbLogo from "../../../../public/skill/database/mongodb-logo.jpg";
+import mongodbLogo from "../../../../public/skill/database/mongodb-logo.png";
 
 import awsLogo from "../../../../public/skill/devops-tool/aws-logo.png";
-import azureLogo from "../../../../public/skill/devops-tool/azure-logo.jpg";
-import dockerLogo from "../../../../public/skill/devops-tool/docker-logo.png";
+import dockerLogo from "../../../../public/skill/devops-tool/docker-logo.webp";
 
 export const skillGroups = [
   {
